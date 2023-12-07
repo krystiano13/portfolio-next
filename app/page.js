6447d8ca1830930b37be7b5c68971a801388a8df
krystiano13 <crystianotv@gmail.com>
2023-12-07 13:32:46 +0100

    create styles for home components

--- app/page.js
+++ app/page.js
@@ -1,21 +1,23 @@
 import {FancyButton} from "@/app/components/FancyButton";
+import './home.css';
 export default function Home() {
     return (
-        <main className="">
-            <h1>Hi, I'm
+        <main className="overflow-hidden ">
+            <h1 id="homeTitle" className="text-text text-head text-center font-semibold text-[5rem] ">
+                {"Hi, I'm "}
                 <span>
-				Krystian Zieja
-			</span>
+				    Krystian Zieja
+			    </span>
             </h1>
-            <h2>
+            <h2 id="homeSecondTitle" className="text-text text-other text-center text-[3rem] font-medium ">
                 Web Developer
                 <label id="underline">_</label>
             </h2>
-            <div className="homeButtons">
-                <a href="/projects">
+            <div className="homeButtons flex gap-[2rem] sm:mb-[15vh]">
+                <a className="decoration-0" href="/projects">
                     <FancyButton>My Projects</FancyButton>
                 </a>
-                <a href="/about">
+                <a className="decoration-0" href="/about">
                     <FancyButton>About Me</FancyButton>
                 </a>
             </div>
