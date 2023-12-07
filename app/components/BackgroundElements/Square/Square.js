514c3e961bd7e868fc4d5233b142a01145ed2e6b
krystiano13 <crystianotv@gmail.com>
2023-12-07 12:35:00 +0100

    create square animations

--- app/components/BackgroundElements/Square/Square.js
+++ app/components/BackgroundElements/Square/Square.js
@@ -1,5 +1,20 @@
+'use client'
+
+import { useEffect, useState } from "react";
+import './Square.css';
+
 export function Square() {
+
+    const [squareAnim, setSquareAnim] = useState(false);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setSquareAnim(true);
+        }, 800);
+    }, []);
+
+
     return (
-        <div className="square w-[5rem] h-[5rem] bg-primary block absolute"></div>
+        <div className={`${squareAnim && 'square-anim'} square w-[5rem] h-[5rem] bg-primary block absolute`}></div>
     )
 }
