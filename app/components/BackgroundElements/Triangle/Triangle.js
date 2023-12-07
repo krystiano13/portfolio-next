adf39a2ca8cbef7dc111f6b111a3b36c548d7a61
krystiano13 <crystianotv@gmail.com>
2023-12-07 12:40:42 +0100

    create triangle animations

--- app/components/BackgroundElements/Triangle/Triangle.js
+++ app/components/BackgroundElements/Triangle/Triangle.js
@@ -1,7 +1,19 @@
+'use client'
+
+import { useEffect, useState } from 'react';
 import './Triangle.css';
 
 export function Triangle() {
+
+    const [triangleAnim, setTriangleAnim] = useState(false);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setTriangleAnim(true);
+        }, 700);
+    }, []);
+
     return (
-        <div className="triangle border-solid absolute"></div>
+        <div className={`${triangleAnim && 'triangle-anim'} triangle border-solid absolute`}></div>
     )
 };
