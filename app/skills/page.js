7cf42a151ef2414cb966a5db61ee84790baa247d
krystiano13 <crystianotv@gmail.com>
2023-12-09 19:23:55 +0100

    create skillData.js file

---
+++ app/skills/page.js
@@ -0,0 +1,14 @@
+import Image from 'next/image';
+export default function Skills() {
+    return (
+        <main
+            className="overflow-hidden w-[100vw] h-[100vh]
+            flex flex-col items-center justify-center gap-[0rem] z-[1]"
+        >
+            <h1>Skills</h1>
+            <div id="skills">
+
+            </div>
+        </main>
+    )
+}
---
+++ app/skills/skillsData.js
@@ -0,0 +1,78 @@
+import astroLogo from "../assets/logos/astro.png";
+import bootstrapLogo from "../assets/logos/bootstrap.png";
+import cssLogo from "../assets/logos/css-3.png";
+import gitLogo from "../assets/logos/git.png";
+import htmlLogo from "../assets/logos/html.png";
+import jsLogo from "../assets/logos/js.png";
+import laravelLogo from "../assets/logos/laravel.png";
+import phpLogo from "../assets/logos/php.png";
+import preactLogo from "../assets/logos/preact.png";
+import pythonLogo from "../assets/logos/Python.svg";
+import qwikLogo from "../assets/logos/qwik.svg";
+import reactLogo from "../assets/logos/react.png";
+import reduxLogo from "../assets/logos/redux.svg";
+import sassLogo from "../assets/logos/sass.png";
+import tsLogo from "../assets/logos/typescript.png";
+
+export const skillsData= [
+    {
+        name: 'Astro',
+        logo: astroLogo
+    },
+    {
+        name: 'Bootstrap',
+        logo: bootstrapLogo
+    },
+    {
+        name: 'CSS',
+        logo: cssLogo
+    },
+    {
+        name: 'GIT',
+        logo: gitLogo
+    },
+    {
+        name: 'HTML',
+        logo: htmlLogo
+    },
+    {
+        name: 'Javascript',
+        logo: jsLogo
+    },
+    {
+        name: 'Laravel',
+        logo: laravelLogo
+    },
+    {
+        name: 'PHP',
+        logo: phpLogo
+    },
+    {
+        name: 'Preact',
+        logo: preactLogo
+    },
+    {
+        name: 'Python',
+        logo: pythonLogo
+    },
+    {
+        name: 'QWIK',
+        logo: qwikLogo
+    },
+    {
+        name: 'React',
+        logo: reactLogo
+    },
+    {
+        name: 'Redux',
+        logo: reduxLogo
+    },
+    {
+        name: 'SASS',
+        logo: sassLogo
+    },
+    {
+        name: 'Typescript',
+        logo: tsLogo
+    },
+];
