97cc4d498165bcf75d6546b611ed16d220918129
krystiano13 <crystianotv@gmail.com>
2023-12-09 09:49:52 +0100

    create styles for elements in about page

--- app/about/page.js
+++ app/about/page.js
@@ -8,9 +8,9 @@ export default function About() {
             className="overflow-hidden w-[100vw] h-[100vh]
             flex flex-col items-center justify-center gap-[0rem] z-[1]"
         >
-           <section id="about" className="text-text w-[50vw] min-w-[25rem] h-[50vh] min-h-[15rem] p-[2rem]">
-               <h1>About Me</h1>
-               <p>
+           <section id="about" className="text-text w-[50vw] min-w-[25rem] h-auto p-[2rem]">
+               <h1 className="text-head text-[3rem] font-semibold">About Me</h1>
+               <p className="text-[1.6rem] max-w-[90%] text-other ">
                    I am a graduate of a technical high school specializing in computer science.
                    After classes, I devoted myself to learning programming,
                    which became my primary hobby at that time.
