87ef4c19906bd9f8798d4f7182365e3864d58c83
krystiano13 <crystianotv@gmail.com>
2023-12-10 13:11:25 +0100

    implement rendering links from an array

---
+++ app/contact/links.js
@@ -0,0 +1,33 @@
+import linkedin from '../assets/contactIcons/linkedin.png';
+import github from '../assets/contactIcons/github.png';
+import mail from '../assets/contactIcons/gmail.png';
+
+export const links = [
+    {
+        id : 0,
+        href: "mailto:crystianotv@gmail.com",
+        src : mail,
+        alt : "mail",
+        name: "Mail",
+        blank: false,
+        noopener: false
+    },
+    {
+        id : 1,
+        href: "https://github.com/krystiano13",
+        src : github,
+        alt : "github",
+        name: "Github",
+        blank: true,
+        noopener: true
+    },
+    {
+        id : 2,
+        href: "https://www.linkedin.com/in/krystian-zieja-1b9902253/",
+        src : linkedin,
+        alt : "linkedin",
+        name: "Linkedin",
+        blank: true,
+        noopener: true
+    },
+];
--- app/contact/page.js
+++ app/contact/page.js
@@ -1,31 +1,24 @@
 import Image from 'next/image';
 import Link from "next/link";
 
-//icons
-import linkedin from '../assets/contactIcons/linkedin.png';
-import github from '../assets/contactIcons/github.png';
-import mail from '../assets/contactIcons/gmail.png';
+import './contact.css';
+import {links} from "@/app/contact/links";
 
 export default function Contact () {
     return (
         <main className="overflow-hidden w-[100vw] h-[100vh] flex flex-col items-center justify-center gap-[0rem] z-[1]">
             <h1>Contact</h1>
-            <section className="contactOption">
-                <Link id="mail" href="mailto:crystianotv@gmail.com" className="contactOption__el">
-                    <Image src={mail} alt="mail"/>
-                    <p>mail</p>
-                    <p id="address" className="hidden">crystianotv@gmail.com</p>
-                </Link>
-                <Link target="_blank" rel="noopener noreferrer" href="https://github.com/krystiano13"
-                   className="contactOption__el">
-                    <Image src={github} alt="github"/>
-                    <p>Github</p>
-                </Link>
-                <Link target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/krystian-zieja-1b9902253/"
-                   className="contactOption__el">
-                    <Image src={linkedin} alt="linkedin"/>
-                    <p>Linkedin</p>
-                </Link>
+            <section className="contactOption w-[50%] min-w-[25rem] mt-[5rem] flex justify-between items-center">
+                {
+                    links.map(item => (
+                        <Link rel={item.noopener && 'noopener noreferrer'} target={item.blank && '_blank'} key={item.id} id="mail" href={item.href}
+                              className="element w-[30%] h-[20%] min-h-[12rem] border-solid border-2 border-text rounded-2xl flex flex-col items-center justify-center cursor-pointer transition decoration-0"
+                        >
+                            <Image src={item.src} alt={item.alt}/>
+                            <p>{item.name}</p>
+                        </Link>
+                    ))
+                }
             </section>
         </main>
     )
