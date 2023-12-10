6f969efa32621a498b83ff03bd6389fe81b6bd3a
krystiano13 <crystianotv@gmail.com>
2023-12-10 16:16:11 +0100

    create title element for projects page

---
+++ app/projects/page.js
@@ -0,0 +1,11 @@
+import './projects.css';
+
+export default function Projects() {
+    return (
+        <main id="projects" className="overflow-hidden w-[100vw] h-[100vh] flex flex-col items-center justify-center gap-[0rem] z-[1]">
+            <h1 className="text-head text-text text-[3rem] w-[60%] min-w-[25rem] relative mt-[3rem]">
+                My Projects
+            </h1>
+        </main>
+    )
+}
