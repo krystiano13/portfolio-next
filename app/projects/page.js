a99f244e154cf3152549febb2251c4d3661a16bd
krystiano13 <crystianotv@gmail.com>
2023-12-10 16:37:54 +0100

    add entry animation in projects page

--- app/projects/page.js
+++ app/projects/page.js
@@ -4,7 +4,7 @@ import {ProjectsWrapper} from "@/app/projects/ProjectsWrapper";
 export default function Projects() {
     return (
         <main id="projects" className="overflow-hidden w-[100vw] h-[100vh] flex flex-col items-center justify-center gap-[0rem] z-[1]">
-            <h1 className="text-head text-text text-[3rem] w-[60%] min-w-[25rem] relative mt-[3rem]">
+            <h1 className="font-semibold text-head text-text text-[3rem] w-[60%] min-w-[25rem] relative mt-[3rem]">
                 My Projects
             </h1>
             <div className="ProjectsWrapper w-[60%] h-auto min-h-[20rem] flex justify-between">
