9edaf9d73aabf65da56fe9c40348bb72f960d1e9
krystiano13 <crystianotv@gmail.com>
2023-12-11 11:22:22 +0100

    create submit button

--- app/contact/form/form.js
+++ app/contact/form/form.js
@@ -7,7 +7,8 @@ export function Form() {
         <form className="flex flex-col w-[80%] md:w-[50%] h-auto mt-[2rem]">
             <input className="mt-[2rem] text-head outline-none p-[1rem] bg-primary bg-opacity-20 text-text" placeholder="Your email" type="email" name="email" />
             <input className="mt-[2rem] text-head outline-none p-[1rem] bg-primary bg-opacity-20 text-text" placeholder="Title" type="text" name="title" />
-            <textarea className="mt-[2rem] text-other outline-none p-[1rem] bg-primary bg-opacity-20 text-text min-h-[18rem] max-h-[18rem]" placeholder="Your Message" name="message"></textarea>
+            <textarea className="mt-[2rem] text-other outline-none p-[1rem] bg-primary bg-opacity-20 text-text min-h-[16rem] max-h-[16rem]" placeholder="Your Message" name="message"></textarea>
+            <button id="submitButton" className="transition-colors mt-[2rem] p-[1rem] text-text bg-primary" type="submit">Send Message</button>
         </form>
     )
 }
