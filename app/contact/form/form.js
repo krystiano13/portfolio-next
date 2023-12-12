4e6da52966a0ed04081e5c60cc19f653fcc5a6c0
krystiano13 <crystianotv@gmail.com>
2023-12-12 13:54:04 +0100

    implement sending email message

--- app/contact/form/form.js
+++ app/contact/form/form.js
@@ -1,10 +1,18 @@
 'use client'
 
 import './form.css';
+import {Loader} from "@/app/components/Loader/Loader";
+import { useState } from "react";
 
 export function Form() {
+
+    const [loading, setLoading] = useState(false);
+    const [sent, setSent] = useState(false);
+    const [success,setSuccess] = useState(false);
+
     const handleSubmit = async (e) => {
         e.preventDefault();
+        setLoading(true);
         const data = new FormData(e.target);
 
         await fetch(
@@ -16,22 +24,45 @@ export function Form() {
         )
             .then(res => res.json())
             .then(data => {
+                setSent(true);
+
                 if(data.result === true) {
-                    alert("Message sent !");
+                    setSuccess(true);
                 }
 
                 else {
-                    alert("Error !");
+                    setSuccess(false);
                 }
+
             })
+        setLoading(false);
     }
 
     return (
         <form onSubmit={handleSubmit} className="flex flex-col w-[80%] md:w-[50%] h-auto mt-[2rem]">
-            <input className="mt-[2rem] text-head outline-none p-[1rem] bg-primary bg-opacity-20 text-text" placeholder="Your email" type="email" name="email" />
-            <input className="mt-[2rem] text-head outline-none p-[1rem] bg-primary bg-opacity-20 text-text" placeholder="Title" type="text" name="subject" />
-            <textarea className="mt-[2rem] text-other outline-none p-[1rem] bg-primary bg-opacity-20 text-text min-h-[16rem] max-h-[16rem]" placeholder="Your Message" name="body"></textarea>
-            <button id="submitButton" className="transition-colors mt-[2rem] p-[1rem] text-text bg-primary" type="submit">Send Message</button>
+            {
+                (loading === false && sent === false) &&
+                <>
+                    <input className="mt-[2rem] text-head outline-none p-[1rem] bg-primary bg-opacity-20 text-text" placeholder="Your email" type="email" name="email" />
+                    <input className="mt-[2rem] text-head outline-none p-[1rem] bg-primary bg-opacity-20 text-text" placeholder="Title" type="text" name="subject" />
+                    <textarea className="mt-[2rem] text-other outline-none p-[1rem] bg-primary bg-opacity-20 text-text min-h-[16rem] max-h-[16rem]" placeholder="Your Message" name="body"></textarea>
+                    <button id="submitButton" className="transition-colors mt-[2rem] p-[1rem] text-text bg-primary" type="submit">Send Message</button>
+                </>
+            }
+            {
+                loading === true &&
+                <div className="w-[100%] h-[25rem] flex justify-center items-center">
+                    <Loader />
+                </div>
+            }
+            {
+                ((sent === true && success === true) && loading === false) &&
+                <p className="info text-center text-text text-head text-[2rem] text-medium">Message was sent successfully</p>
+            }
+            {
+                ((sent === true && success === false) && loading === false) &&
+                <p className="info text-center text-text text-head text-[2rem] text-medium">Something went wrong</p>
+            }
         </form>
     )
 }
--- app/loading.js
+++ app/loading.js
@@ -2,7 +2,7 @@ import {Loader} from "@/app/components/Loader/Loader";
 
 export default function Loading() {
     return (
-        <div className="loader w-[100vw] h-[100vh] flex justify-center items-center">
+        <div className="loader w-full h-full flex justify-center items-center">
             <Loader />
         </div>
     )
