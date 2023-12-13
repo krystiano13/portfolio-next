17d67de90e91d9ff052f847a6839a79c0104d927
krystiano13 <crystianotv@gmail.com>
2023-12-13 20:26:29 +0100

    fix mobile view in contact page

--- app/contact/form/form.js
+++ app/contact/form/form.js
@@ -45,7 +45,7 @@ export function Form() {
                 <>
                     <input className="mt-[2rem] text-head outline-none p-[1rem] bg-primary bg-opacity-20 text-text" placeholder="Your email" type="email" name="email" />
                     <input className="mt-[2rem] text-head outline-none p-[1rem] bg-primary bg-opacity-20 text-text" placeholder="Title" type="text" name="subject" />
-                    <textarea className="mt-[2rem] text-other outline-none p-[1rem] bg-primary bg-opacity-20 text-text min-h-[16rem] max-h-[16rem]" placeholder="Your Message" name="body"></textarea>
+                    <textarea className="mt-[2rem] text-other outline-none p-[1rem] bg-primary bg-opacity-20 text-text min-h-[12rem] max-h-[12rem] md:min-h-[16rem] md:max-h-[16rem]" placeholder="Your Message" name="body"></textarea>
                     <button id="submitButton" className="transition-colors mt-[2rem] p-[1rem] text-text bg-primary" type="submit">Send Message</button>
                 </>
             }
