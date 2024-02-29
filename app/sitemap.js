91498a915828c80db5e336e4ec7cc3c419fe5b74
krystiano13 <crystianotv@gmail.com>
2024-02-29 17:43:20 +0100

    add all urls to the sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -3,19 +3,37 @@ import { MetadataRoute } from 'next'
 export default function sitemap() {
     return [
         {
-            url: 'https://acme.com',
+            url: 'https://krystian-zieja.vercel.app/',
             lastModified: new Date(),
-            changeFrequency: 'yearly',
+            changeFrequency: 'weekly',
             priority: 1,
         },
         {
-            url: 'https://acme.com/about',
+            url: 'https://krystian-zieja.vercel.app/projects',
             lastModified: new Date(),
-            changeFrequency: 'monthly',
+            changeFrequency: 'weekly',
             priority: 0.8,
         },
         {
-            url: 'https://acme.com/blog',
+            url: 'https://krystian-zieja.vercel.app/about',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.5,
+        },
+        {
+            url: 'https://krystian-zieja.vercel.app/skills',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.5,
+        },
+        {
+            url: 'https://krystian-zieja.vercel.app/contact',
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.5,
+        },
+        {
+            url: 'https://krystian-zieja.vercel.app/contact/form',
             lastModified: new Date(),
             changeFrequency: 'weekly',
             priority: 0.5,
